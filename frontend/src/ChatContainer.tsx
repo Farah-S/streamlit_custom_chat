768398796cb105f1686d5b256cf4b23d9ec0a71c
Farah Sherif <farah.faroha132@gmail.com>
2023-12-09 18:20:20 +0200

    layout adjustments

--- frontend/src/ChatContainer.tsx
+++ frontend/src/ChatContainer.tsx
@@ -15,8 +15,35 @@ document.body.style.backgroundColor = "transparent";
 
 const ChatContainer=(props: ComponentProps)=>{
   var {messages}: ChatContainerArgs = props.args;
+  const paperStyle={
+              '&::-webkit-scrollbar': {
+                width: '0.4em',
+              },
+              '&::-webkit-scrollbar-track': {
+                scrollMarginTop: "50px",
+                boxShadow: 'inset 0 0 6px rgba(0,0,0,0.0)',
+                webkitBoxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)'
+              },
+              '&::-webkit-scrollbar-thumb': {
+                backgroundColor:"#fdfaff",
+                // backgroundColor: 'rgba(0,0,0,0)',
+                outline: '1px solid black',
+                borderRadius: 8,
+              },
+              overflowY:"scroll",
+              p: 2,
+              // marginBottom:"0",
+              height:"550px",
+              borderRadius:"2rem",
+              marginLeft:"5%",
+              // marginRight:"25px",
+              boxShadow:"inset 0px 0 20px 5px rgb(219 219 219 / 11%), 0px 0px 0px 0px rgb(0 0 0 / 8%), 0px 1px 3px 0px rgb(0 0 0 / 0%)",
+              // flexGrow: 20,
+              backgroundColor: '#fdfaff',
+              // width:"85%"
+            }
+
   const chatBubbles=[]
-  
   for (let index = 0; index < messages.length; index++) {
     chatBubbles.push(<ChatBubble data={{"role":messages[index]["role"],"content":messages[index]["content"]}} key={messages[index]["key"]} />);
   }
@@ -25,22 +52,13 @@ const ChatContainer=(props: ComponentProps)=>{
   
   return (
     <Row>
-      <Col xs={1}>
-        <button style={{width:"100px"}}/>
-        <button style={{width:"100px"}}/>
+      <Col xs={1} style={{width:"10%"}}>
+        <button style={{width:"80%"}}/>
+        <button style={{width:"80%"}}/>
       </Col>
-      <Col  xs={11}>
+      <Col  xs={11} style={{width:"90%"}}>
         <Paper
-            sx={{
-              p: 2,
-              minHeight:"550px",
-              borderRadius:"2rem",
-              marginLeft:"25px",
-              marginRight:"25px",
-              boxShadow:"inset 0px 0 20px 5px rgb(219 219 219 / 11%), 0px 0px 0px 0px rgb(0 0 0 / 8%), 0px 1px 3px 0px rgb(0 0 0 / 0%)",
-              flexGrow: 20,
-              backgroundColor: '#fdfaff'
-            }}
+            sx={paperStyle}
           >
           {chatBubbles}
         </Paper>
--- frontend/src/CustomChatBubble.tsx
+++ frontend/src/CustomChatBubble.tsx
@@ -9,41 +9,45 @@ import Col from 'react-bootstrap/Col';
 function ChatBubble({data:{role="",content=""}}, key:string){
 
   const BubbleStyle = {
-      color: "#9093D8",
-      backgroundColor: role==="user"?"#f1f8ff":"#faf4ff",
-      padding: "12px",
-      paddingTop:"5px",
-      paddingBottom:"5px",
+      color: "#7377cb",
+      backgroundColor: role==="user"?"#eef7ff":"#f8f2ff",
+      // padding: "12px",
+      paddingTop:"7px",
+      paddingRight:"10px",
+      paddingLeft:"10px",
+      paddingBottom:"7px",
       "fontWeight":"100",
       maxWidth:"90%",
+      // width:"fit-content",
       borderRadius:"2rem"
     };
 
   const userContainerStyle = {
-      width:"93%", 
+      width:"98%", 
       paddingTop:"10px", 
-      paddingBottom:"8px", 
+      paddingBottom:"10px", 
       height: "fit-content", 
       justifyContent:"center"
     };
 
   const assistantContainerStyle = {
-      width:"93%", 
+      width:"98%", 
       paddingTop:"10px", 
-      paddingBottom:"8px", 
+      paddingBottom:"10px", 
       backgroundColor:"transparent", 
-      marginLeft: "20px", 
+      marginLeft: "5px", 
       height: "fit-content", 
       justifySelf:"end"
     };
   
   useEffect(() => Streamlit.setFrameHeight());
-
+  // TODO: only display image if prev message was of a diff user type, can add "prev" var that is sent to
+  // the container from the app.py
   if(role==="assistant"){
     return (
       <Container key={key} fluid style={assistantContainerStyle}>
         <Row style={{backgroundColor:"transparent"}}>
-          <Col xs={1} style={{backgroundColor:"transparent", paddingRight:"15px", marginRight: "10px",paddingTop: "3px"}}>
+          <Col xs={1} style={{backgroundColor:"transparent",width:"6%", paddingLeft:"5px", marginLeft: "30px",paddingTop: "2px"}}>
               <img alt="icon" src={require('./assets/images/robot_icon.png')} height={35} />
           </Col>
           <Col xs={10} style={BubbleStyle}>
@@ -63,12 +67,12 @@ function ChatBubble({data:{role="",content=""}}, key:string){
         <Col xs={1}>
         </Col>
         <Col xs={10} style={BubbleStyle}>
-          <Typography style={{whiteSpace:"pre-line",fontFamily: "itim", fontWeight: "525", wordWrap: "break-word",  padding: "5px",}}>
+          <Typography style={{textAlign:"right",whiteSpace:"pre-line",fontFamily: "itim", fontWeight: "525", wordWrap: "break-word",  padding: "5px",}}>
                 {content}
           </Typography>
         </Col>
         <Col xs={1}>
-            <img alt="icon" src={require('./assets/images/person_icon.png')} height={45} />
+            <img alt="icon" src={require('./assets/images/person_icon.png')} height={43} />
         </Col>
       </Row>
     </Container>
