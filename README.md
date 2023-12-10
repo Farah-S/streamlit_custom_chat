c63114785a2d4792bb5ed754dbff5880b00f994d
Farah Sherif <farah.faroha132@gmail.com>
2023-12-10 22:02:23 +0200

    preparing for publish

---
+++ README.md
@@ -0,0 +1,21 @@
+# streamlit-custom-component
+
+Streamlit component that displays a custom made chat layout
+
+## Installation instructions
+
+```sh
+pip install streamlit-custom-chat
+```
+
+## Usage instructions
+
+```python
+import streamlit as st
+
+from streamlit-custom-chat import ChatContainer
+
+value = ChatContainer()
+
+st.write(value)
+```
--- streamlit_custom_chat/__init__.py
+++ streamlit_custom_chat/__init__.py
@@ -1,14 +1,19 @@
 import streamlit.components.v1 as components
+import os
 
 # Create a function _component_func which will call the frontend component when run
-_component_func = components.declare_component(
-    "ChatContainer",
-    url="http://localhost:3000",  # Fetch frontend component from local webserver
-)
+# _component_func = components.declare_component(
+#     "ChatContainer",
+#     url="http://localhost:3000",  # Fetch frontend component from local webserver
+# )
+
+parent_dir = os.path.dirname(os.path.abspath(__file__))
+build_dir = os.path.join(parent_dir, "frontend/build")
+component = components.declare_component("ChatContainer", path=build_dir)
 
 # Define a public function for the package,
 # which wraps the caller to the frontend code
 
 def ChatContainer(messages=[], key=None) -> int:
-    component_value = _component_func(messages=messages, key=key)
+    component_value = component(messages=messages, key=key)
     return component_value
