7a9cfb03d9fefcfe358cb7219988b3d85984a439
Farah Sherif <farah.faroha132@gmail.com>
2023-12-15 14:24:32 +0200

    latest published version

--- build/lib/streamlit_custom_chat/__init__.py
+++ build/lib/streamlit_custom_chat/__init__.py
@@ -1,30 +1,76 @@
-import streamlit.components.v1 as components
 import os
-from pathlib import Path
-# Create a function _component_func which will call the frontend component when run
-# _component_func = components.declare_component(
-#     "ChatContainer",
-#     url="http://localhost:3000",  # Fetch frontend component from local webserver
-# )
+import streamlit.components.v1 as components
 
-_USE_WEB_DEV_SERVER = os.getenv("USE_WEB_DEV_SERVER", False)
-_WEB_DEV_SERVER_URL = os.getenv("WEB_DEV_SERVER_URL", "http://localhost:3000")
-COMPONENT_NAME = "ChatContainer"
+# Create a _RELEASE constant. We'll set this to False while we're developing
+# the component, and True when we're ready to package and distribute it.
+# (This is, of course, optional - there are innumerable ways to manage your
+# release process.)
+_RELEASE = True
 
-if _USE_WEB_DEV_SERVER:
-    component = components.declare_component(name=COMPONENT_NAME, url=_WEB_DEV_SERVER_URL)
+# Declare a Streamlit component. `declare_component` returns a function
+# that is used to create instances of the component. We're naming this
+# function "_component_func", with an underscore prefix, because we don't want
+# to expose it directly to users. Instead, we will create a custom wrapper
+# function, below, that will serve as our component's public API.
+
+# It's worth noting that this call to `declare_component` is the
+# *only thing* you need to do to create the binding between Streamlit and
+# your component frontend. Everything else we do in this file is simply a
+# best practice.
+
+if _RELEASE==False:
+    _component_func = components.declare_component(
+        # We give the component a simple, descriptive name ("ChatContainer"
+        # does not fit this bill, so please choose something better for your
+        # own component :)
+        "ChatContainer",
+        # Pass `url` here to tell Streamlit that the component will be served
+        # by the local dev server that you run via `npm run start`.
+        # (This is useful while your component is in development.)
+        url="http://localhost:3001",
+    )
 else:
-    build_dir = str(Path(__file__).parent / "frontend" / "build")
-    component = components.declare_component(name=COMPONENT_NAME, path=build_dir)
+    # When we're distributing a production version of the component, we'll
+    # replace the `url` param with `path`, and point it to the component's
+    # build directory:
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(parent_dir, "frontend\\build")
+    print(build_dir)
+    _component_func = components.declare_component("ChatContainer", path=build_dir)
 
 
-# parent_dir = os.path.dirname(os.path.abspath(__file__))
-# build_dir = os.path.join(parent_dir, "frontend/build")
-# component = components.declare_component("ChatContainer", path=build_dir)
+# Create a wrapper function for the component. This is an optional
+# best practice - we could simply expose the component function returned by
+# `declare_component` and call it done. The wrapper allows us to customize
+# our component's API: we can pre-process its input args, post-process its
+# output value, and add a docstring for users.
+def ChatContainer(messages=[], key=None):
+    """Create a new instance of "ChatContainer".
 
-# Define a public function for the package,
-# which wraps the caller to the frontend code
+    Parameters
+    ----------
+    name: str
+        The name of the thing we're saying hello to. The component will display
+        the text "Hello, {name}!"
+    key: str or None
+        An optional key that uniquely identifies this component. If this is
+        None, and the component's arguments are changed, the component will
+        be re-mounted in the Streamlit frontend and lose its current state.
 
-def ChatContainer(messages=[], key=None) -> int:
-    component_value = component(messages=messages, key=key)
+    Returns
+    -------
+    None
+    """
+    # Call through to our private component function. Arguments we pass here
+    # will be sent to the frontend, where they'll be available in an "args"
+    # dictionary.
+    #
+    # "default" is a special argument that specifies the initial return
+    # value of the component before the user has interacted with it.
+    
+    # chatBubbles=[]
+    component_value = _component_func(messages=messages, key=key)
+
+    # We could modify the value returned from the component if we wanted.
+    # There's no need to do this in our simple example - but it's an option.
     return component_value
--- streamlit_custom_chat/frontend/src/index.tsx
+++ streamlit_custom_chat/frontend/src/index.tsx
@@ -1,21 +1,10 @@
-//file that renders the react components
-import React from "react";
-import ReactDOM from "react-dom";
-import ChatContainer from "./ChatContainer";
-import { Client as Styletron } from "styletron-engine-atomic";
-import { Provider as StyletronProvider } from "styletron-react";
-import { ThemeProvider, LightTheme } from "baseui";
-
-const engine = new Styletron();
+import React from "react"
+import ReactDOM from "react-dom"
+import ChatContainer from "./ChatContainer"
 
 ReactDOM.render(
-  
   <React.StrictMode>
-    <StyletronProvider value={engine}>
-      <ThemeProvider theme={LightTheme}>
-        <ChatContainer/>
-      </ThemeProvider>
-    </StyletronProvider>
+    <ChatContainer />
   </React.StrictMode>,
   document.getElementById("root")
-);
+)
