af3344ebc45d5d44f03a552e4c53370df1a9b9a0
Farah Sherif <farah.faroha132@gmail.com>
2023-12-17 21:17:47 +0200

    comment change

--- streamlit_custom_chat/__init__.py
+++ streamlit_custom_chat/__init__.py
@@ -20,9 +20,7 @@ _RELEASE = True
 
 if _RELEASE==False:
     _component_func = components.declare_component(
-        # We give the component a simple, descriptive name ("ChatContainer"
-        # does not fit this bill, so please choose something better for your
-        # own component :)
+        # We give the component a simple, descriptive name ("ChatContainer")
         "ChatContainer",
         # Pass `url` here to tell Streamlit that the component will be served
         # by the local dev server that you run via `npm run start`.
